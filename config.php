c7578df83e09dc4aa86d30e80517c35616d13fcc
kirakirai8023 <kirakirai8023@gmail.com>
2024-07-26 02:13:23 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// PayPal API configuration information
+return [
+    'client_id' => 'your_paypal_client_id', // Replace with your PayPal client ID
+    'client_secret' => 'your_paypal_client_secret', // Replace with your PayPal client secret
+    'sandbox' => true, // Set to true for sandbox environment, false for production environment
+];
